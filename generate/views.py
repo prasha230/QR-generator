0056ceb5e0ef44231e316c71cf029ebd1a96e237
Prashant Rai <prashantrai230@gmail.com>
2022-08-24 01:23:00 +0530

    Url checker added

--- generate/views.py
+++ generate/views.py
@@ -2,13 +2,17 @@ from django.shortcuts import render
 from .forms import textinput
 import qrcode
 from PIL import Image
+import re
 # Create your views here.
 def home(request):
     if request.method == 'POST':
         form=textinput(request.POST)
         if form.is_valid():
             inp=form.cleaned_data['inp']
+            url_pattern_1 = "^https?:\\/\\/(?:www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b(?:[-a-zA-Z0-9()@:%_\\+.~#?&\\/=]*)$"
+            url_pattern_2 = "^[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b(?:[-a-zA-Z0-9()@:%_\\+.~#?&\\/=]*)$"
+            flag=(re.match(url_pattern_1, inp)!=None) or (re.match(url_pattern_2, inp)!=None)
             qrcode.make(inp).save('generate/static/qr.png')
-            return render(request,'generate/result.html',{'inp':inp})
+            return render(request,'generate/result.html',{'inp':inp,'flag':flag})
     form=textinput()
     return render(request,'generate/home.html',{'form':form})
