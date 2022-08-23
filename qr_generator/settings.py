2f0593fa3d03ea5822ee55cd7d379eb6e9689a4c
Prashant Rai <prashantrai230@gmail.com>
2022-08-24 01:04:17 +0530

    static changes

--- qr_generator/settings.py
+++ qr_generator/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-1w+a&hvp0_f0ty(x43)cet*0$8_uvm0i!1i(g04^+h%scnzk3f"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['qr-generator1.herokuapp.com','127.0.0.1']
 
@@ -120,7 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
@@ -129,4 +129,3 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 db_from_env = dj_database_url.config(conn_max_age = 600)
-django_heroku.settings(locals())
